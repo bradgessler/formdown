d642a7a63e30d2f1ea1d30b44722dbd4e76d8502
Brad Gessler <brad@polleverywhere.com>
2014-08-02 00:21:38 -0700

    Detect presence of piped input and return error.

--- lib/formdown/cli.rb
+++ lib/formdown/cli.rb
@@ -4,6 +4,10 @@ require 'thor'
 class Formdown::CLI < Thor
   desc "render", "Render formdown file to html"
   def render
-    puts Formdown::Renderer.new($stdin.read).to_html 
+    if $stdin.tty?
+      error "Pipe Formdown into this command. Example: `$ echo 'Email: ____@(Email)' | formdown render`"
+    else
+      puts Formdown::Renderer.new($stdin.read).to_html 
+    end
   end
 end
