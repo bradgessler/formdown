af365dd100ae821bee28a4ba6a8ff5ed80d41bd0
Brad Gessler <brad@polleverywhere.com>
2014-09-14 17:59:34 -0700

    Parse radio button labels.

--- lib/kramdown/parser/formdown/radio_button_field.rb
+++ lib/kramdown/parser/formdown/radio_button_field.rb
@@ -1,13 +1,36 @@
 module Kramdown
   module Parser
     class Formdown < Kramdown::Parser::Kramdown
-      RADIO_BUTTON_FIELD_START = /\(([\sxX*])?\)/
+      # Radio buttons may be "checked" with (*), (x).
+      RADIO_BUTTON_CHECKED = /[x*]/i
+
+      # Radio buttons begin with a () and may optionally be checked or have a space.
+      RADIO_BUTTON_FIELD_START = /\((?:#{RADIO_BUTTON_CHECKED}|\s)?\)/
+
+      # A radio button label is any text after a radio button up to the next radio button or a new line.
+      RADIO_BUTTON_FIELD_LABEL = /(.*?)(?=#{RADIO_BUTTON_FIELD_START}|\n|\Z)/
 
       def parse_radio_buttons
-        @src.pos += @src.matched_size
-        @tree.children << Element.new(:html_element, :input, type: :radio)
+        # Get a DOM id for labels and such.
+        dom_id = next_dom_id
+
+        # Find the start of the radio button.
+        @src.scan(RADIO_BUTTON_FIELD_START)
+        @tree.children << Element.new(:html_element, :input, type: :radio, id: dom_id)
+
+        # Read the radio button label.
+        if label = @src.scan(RADIO_BUTTON_FIELD_LABEL)
+          @tree.children << Element.new(:html_element, :label, for: dom_id)
+          @tree.children.last.children << Element.new(:text, label.strip)
+        end
       end
       define_parser(:radio_buttons, RADIO_BUTTON_FIELD_START)
+
+      # Generates IDs for labels
+      def next_dom_id
+        @id ||= 0
+        [:radio, @id+=1].join("_")
+      end
     end
   end
 end
--- spec/lib/kramdown/parser/formdown/radio_button_field_spec.rb
+++ spec/lib/kramdown/parser/formdown/radio_button_field_spec.rb
@@ -1,32 +1,60 @@
 require 'spec_helper'
 
 describe Kramdown::Parser::Formdown do
+  context "regexp", focus: false do
+    context "RADIO_BUTTON_FIELD_START" do
+      subject { Kramdown::Parser::Formdown::RADIO_BUTTON_FIELD_START }
+      it "matches () for start of radio button" do
+        expect("(*) One".match(subject).captures).to match([])
+      end
+    end
+
+    context "RADIO_BUTTON_FIELD_LABEL" do
+      subject { Kramdown::Parser::Formdown::RADIO_BUTTON_FIELD_LABEL }
+
+      it "matches radio button label to end of line" do
+        expect(" One\n Two ()".match(subject).captures).to match([" One"])
+      end
+
+      it "matches radio button label to next radio button" do
+        expect(" One () Two ()".match(subject).captures).to match([" One "])
+      end
+    end
+  end
+
   context "radio button" do
-    subject { html 'kitchen_sink.fmd' }
+    subject { html 'radio.fmd' }
     context "single line" do
       it "renders radio button input" do
-        expect(subject).to include(%(<input type="radio"></input>))
+        expect(subject).to include(%(<input type="radio" id="radio_1"></input>))
       end
+      it "renders label" do
+        expect(subject).to include(%(<label for="radio_1">Eat a potato</label>))
+        expect(subject).to include(%(<label for="radio_2">Bang my head against the all</label>))
+        expect(subject).to include(%(<label for="radio_3">Do jumping jacks</label>))
+      end
+
       # TODO - Group radio buttons and infer some sort of name that can
       # be mapped back into the document... like `radio-#{radio_instance}`
       # where `seq` monotonically increases per named radio button group.
       #
       # TODO - Does `radio[0]` blow up Sinatra/Rails style parsers? Should it be
       # radio_0?
-      it "renders radio button name" do
-        pending "parse multiple lines of radio buttons"
-        expect(subject).to include('<input type="radio" name="radio[0]" id="radio_0_0"></input>')
-      end
-      # TODO - Name radio buttons based on the instance of the group AND the sequence of the
-      # radio button (e.g. is it the first or last option to choose from?)
-      it "renders labels" do
-        pending "render labels"
-        expect(subject).to include('<label for="radio_0_0">Eat a potato</label>')
-      end
-      it "is checked" do
-        pending "check forms with *, x, or X"
-        expect(subject).to include('<input type="radio" name="radio[0]" id="radio_0_1" checked></input>')
-      end
+
+      # it "renders radio button name" do
+      #   pending "parse multiple lines of radio buttons"
+      #   expect(subject).to include('<input type="radio" name="radio[0]" id="radio_0_0"></input>')
+      # end
+      # # TODO - Name radio buttons based on the instance of the group AND the sequence of the
+      # # radio button (e.g. is it the first or last option to choose from?)
+      # it "renders labels" do
+      #   pending "render labels"
+      #   expect(subject).to include('<label for="radio_0_0">Eat a potato</label>')
+      # end
+      # it "is checked" do
+      #   pending "check forms with *, x, or X"
+      #   expect(subject).to include('<input type="radio" name="radio[0]" id="radio_0_1" checked></input>')
+      # end
     end
   end
 end
